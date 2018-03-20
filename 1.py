3c3b7fc22a101d1092c4c48bfa5a55c152b55422
MaxIo Yu <maxio.l.yu@gmail.com>
2018-03-20 22:25:17 +0800

    1.3

--- 1.py
+++ 1.py
@@ -10,14 +10,9 @@ class Solution:
         dict = {}
         length = len(nums)
         for i in range(length):
-            dict[nums[i]] = i
-
-        for i in range(length):
             if target-nums[i] in dict:
-                if dict[target-nums[i]] != i:
-                    return [i,dict[target-nums[i]]]
-
-
+                return [dict[target-nums[i]], i]
+            dict[nums[i]] = i
 
 print (Solution().twoSum([2,3,4],6))
 
