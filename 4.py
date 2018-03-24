17b1f7970f28bf6cb6634ef0a2de93eec5438277
MaxIo Yu <maxio.l.yu@gmail.com>
2018-03-24 23:27:18 +0800

    4.2

--- 4.py
+++ 4.py
@@ -7,27 +7,57 @@ class Solution:
         :type nums2: List[int]
         :rtype: float
         """
-        merged = []
-        while (1):
-            if nums1 and nums2:
-                if nums1[0] < nums2[0]:
-                    merged.append(nums1.pop(0))
-                else:
-                    merged.append(nums2.pop(0))
-            elif nums1:
-                merged.extend(nums1)
-                break
-            elif nums2:
-                merged.extend(nums2)
+        m = len(nums1)
+        n = len(nums2)
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
+
+
+        imin, imax = 0, m
+        half,odd = divmod(m+n,2)
+        while(imin <= imax):
+            i = (imin+imax)//2
+            j = half - i
+            # left_A has element
+            if i > 0 and nums1[i-1] > nums2[j]:
+                imax = i - 1
+            # left_B has element
+            elif i < m and nums2[j-1] > nums1[i]:
+                imin = i + 1
+            else:
                 break
 
-        mid, odd = divmod(len(merged),2)
-        if odd:
-            return merged[mid]
+        j = half - i
+
+        # A has no right part
+        if i == m:
+            min_right = nums2[j]
+        # B has no right part
+        elif j == n:
+            min_right = nums1[i]
         else:
-            return (merged[mid] + merged[mid-1])/2
+            min_right = min(nums1[i], nums2[j])
+
+        if odd:
+            return min_right
+        else:
+            # A has no left part
+            if i == 0:
+                return (nums2[j-1] + min_right)/2
+            # B has no left part
+            elif j == 0:
+                return (nums1[i-1] + min_right)/ 2
+            else:
+                return (max(nums1[i-1],nums2[j-1]) + min_right)/ 2
 
 
-a=[1,3,5]
-b=[2,4,6]
-Solution().findMedianSortedArrays(a,b)
+
+a=[6,7,8]
+b=[1,2,3,4,5]
+a=[1,3]
+b=[2]
+a=[]
+b=[3,4]
+a=[1,2]
+b=[3,4]
+print(Solution().findMedianSortedArrays(a,b))
